adf67f6adbbab0800f947715c82a0a0f13306755
Dylan <dtyott@gmail.com>
2022-08-29 19:10:46 -0400

    first real commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+import { React, useEffect, useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import intro from './rooms/intro';
+import treasure from './rooms/treasure';
+
+const color_dict = [
+  {"token":"treasure", "color":"gold"},
+]
+const default_color = "blue"
+
 function App() {
+
+  useEffect(() => {
+    const url = window.location.href
+    const possibleColors = color_dict.filter(d=>url.toLowerCase().includes(d.token))
+    const color = possibleColors.length>0? possibleColors[0].color : default_color
+    document.body.style = 'background: '+color+';';
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to be a really good boy.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        
+        <Routes>
+        <Route exact path="/" element={intro()} />
+        <Route path="/treasure" element={treasure()} />
+
+
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/rooms/treasure.jsx
@@ -0,0 +1,20 @@
+import {React, useState} from 'react';
+import { Grid, Paper} from "@mui/material";
+import { Link } from '@mui/material';
+
+
+const SCREEN_WIDTH=12
+
+export default function treasure() {
+    
+
+
+
+    return  <div>
+        <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center', color: 'white'}}>
+        You have found the treasure!!
+        </h2>
+        
+        
+            </div> 
+  }
